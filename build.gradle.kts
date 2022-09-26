155b77cf5162c5bdc86dc91d762aa3e8525b7df2
Darwin Esteban Algarin Sarmiento <darwin.algarin@rappi.com>
2022-09-26 09:42:25 -0500

    Added base code.

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.intellij") version "1.9.0"
+}
+
+group = "co.anbora.labs"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2021.3.3")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("213")
+        untilBuild.set("222.*")
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
