154da78264eea48e7ea7f5f1e20d6e591866e90e
Darwin Esteban Algarin Sarmiento <darwin.algarin@rappi.com>
2023-01-20 19:31:24 -0500

    changed type to IU.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ sourceSets {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("LATEST-EAP-SNAPSHOT")
-    type.set("IC") // Target IDE Platform
+    type.set("IU") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
 }
