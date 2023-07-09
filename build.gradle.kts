c9f1b1d73cb3723c6350fb0180a504280c06922d
Darwin Algarin <dalgarins@live.com>
2023-07-09 18:13:35 -0500

    upgrade sdk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.14.2"
+    id("org.jetbrains.intellij") version "1.15.0"
     id("org.jetbrains.grammarkit") version "2022.3.1"
 }
 
 group = "co.anbora.labs"
-version = "1.3.8"
+version = "1.3.9"
 
 repositories {
     mavenCentral()
