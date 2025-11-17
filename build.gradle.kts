4cb5887406f58e340edc4829eeae2d84c69cd57f
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2025-11-17 14:35:58 +0000

    upgrade to sdk 253.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ fun environment(key: String) = providers.environmentVariable(key)
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.2.0"
-    id("org.jetbrains.intellij.platform") version "2.9.0"
-    id("org.jetbrains.grammarkit") version "2022.3.2.2"
+    id("org.jetbrains.intellij.platform") version "2.10.4"
+    id("org.jetbrains.grammarkit") version "2023.3.0.1"
 }
 
 group = properties("pluginGroup").get()
--- src/main/kotlin/co/anbora/labs/brewbundle/ide/startup/LicenseStartup.kt
+++ src/main/kotlin/co/anbora/labs/brewbundle/ide/startup/LicenseStartup.kt
@@ -3,13 +3,17 @@ package co.anbora.labs.brewbundle.ide.startup
 import co.anbora.labs.brewbundle.license.CheckLicense
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.util.concurrency.AppExecutorUtil
+import java.util.concurrent.TimeUnit
 
 class LicenseStartup: ProjectActivity {
     override suspend fun execute(project: Project) {
-        val licensed = CheckLicense.isLicensed() ?: false
+        AppExecutorUtil.getAppScheduledExecutorService().schedule({
+            val licensed = CheckLicense.isLicensed() ?: false
 
-        if (!licensed) {
-            CheckLicense.requestLicense("Support plugin.")
-        }
+            if (!licensed && !project.isDisposed) {
+                CheckLicense.requestLicense("Support plugin.")
+            }
+        }, 5, TimeUnit.MINUTES)
     }
 }
